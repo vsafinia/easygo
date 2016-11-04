dfc6dcc614721e110b85b4fa2378407055884505
vsafinia <v.safinia@gmail.com>
2016-11-04 15:26:32 +1300

    Add files via upload

---
+++ config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://easygo.rameshchaudhary.com.np/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://easygo.rameshchaudhary.com.np/');
+
+// DIR
+define('DIR_APPLICATION', '/home/rameshchaudhary/public_html/easygo/catalog/');
+define('DIR_SYSTEM', '/home/rameshchaudhary/public_html/easygo/system/');
+define('DIR_IMAGE', '/home/rameshchaudhary/public_html/easygo/image/');
+define('DIR_LANGUAGE', '/home/rameshchaudhary/public_html/easygo/catalog/language/');
+define('DIR_TEMPLATE', '/home/rameshchaudhary/public_html/easygo/catalog/view/theme/');
+define('DIR_CONFIG', '/home/rameshchaudhary/public_html/easygo/system/config/');
+define('DIR_CACHE', '/home/rameshchaudhary/public_html/easygo/system/storage/cache/');
+define('DIR_DOWNLOAD', '/home/rameshchaudhary/public_html/easygo/system/storage/download/');
+define('DIR_LOGS', '/home/rameshchaudhary/public_html/easygo/system/storage/logs/');
+define('DIR_MODIFICATION', '/home/rameshchaudhary/public_html/easygo/system/storage/modification/');
+define('DIR_UPLOAD', '/home/rameshchaudhary/public_html/easygo/system/storage/upload/');
+
+// DB
+
+// DB
+    define('DB_DRIVER', 'mysqli');
+    define('DB_HOSTNAME', 'localhost');
+    define('DB_USERNAME', 'rameshch_eg');
+    define('DB_PASSWORD', 'tQahXfT2Vp$k');
+    define('DB_DATABASE', 'rameshch_eg');
+    define('DB_PORT', '3306');
+    define('DB_PREFIX', 'rtc_');
+
